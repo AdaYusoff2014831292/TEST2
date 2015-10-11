797f2201934da7824b3917f5b95b26cf7f2b310c
AdaYusoff2014831292 <adayusoff@yahoo.com>
2015-10-11 16:18:31 +0800

    TEST2
    COMPANY A and COMPANY B

---
+++ company_/COMPANY.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class COMPANY here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.net.*;
+import java.io.*;
+
+public class COMPANY
+{
+    public static void main(String[] args){
+        
+    InetAddress ip;
+    try {
+            
+        ip = InetAddress.getLocalHost();
+        System.out.println("Current IP address : " + ip.getHostAddress());
+        
+        NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            
+        byte[] mac = network.getHardwareAddress();
+            
+        System.out.print("Current MAC address : ");
+            
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mac.length; i++) {
+            sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));        
+        }
+        System.out.println(sb.toString());
+            
+    } catch (UnknownHostException e) {
+        
+        e.printStackTrace();
+        
+    } catch (SocketException e){
+            
+        e.printStackTrace();
+            
+    } 
+   }
+
+}

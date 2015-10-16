eb0b6d48c51260ae9638ea0c047803f108b94264
AdaYusoff2014831292 <adayusoff@yahoo.com>
2015-10-17 02:10:22 +0800

    test2
    python

--- python/get mac addr.py
+++ python/get mac addr.py
@@ -1,7 +1,11 @@
 import re, uuid
 print "Mac Addr:" + ':'.join(re.findall('..', '%012x' % uuid.getnode()))
    
-
+def get_ip_6(host, port=0):
+    import socket
+    
+    result = socket.getaddrinfo(host, port, socket.AF_INET6)
+    return result
 
 
 import threading
